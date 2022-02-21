c1e4f53e2ebd42100dbf42a56389f815dfb8fbfb
Baptiste ROYER <56670984+BaptisteRoyer@users.noreply.github.com>
2022-02-21 18:32:17 +0100

    Added css, updated script

--- js/script.js
+++ js/script.js
@@ -3,3 +3,42 @@ var reconnectTimeout = 2000;
 var host = "192.168.144.154"; //change this according to your brokers IP
 var port = 9001;
 
+function onMessageArrived(r_message)
+{
+    out_msg = r_message.payloadString;
+
+    if(r_message.destinationName == "carSecurityState")
+    {
+        console.log("ALED ON VOLE MON AUTO");
+    }
+
+}
+
+function onConnect()
+{
+    // Once a connection has been made, make a subscription and send a message.
+
+    console.log("Connected");
+    mqtt.subscribe("carSecurityState");
+    sendParams();
+
+}
+
+function MQTTconnect()
+{
+
+    console.log("connecting to " + host + " " + port);
+    mqtt = new Paho.MQTT.Client(host, port, "clientJSSubscriber");
+    //document.write("connecting to "+ host);
+    var options = 
+    {
+
+        timeout: 3,
+        onSuccess: onConnect
+
+    };
+
+    mqtt.onMessageArrived = onMessageArrived;
+    mqtt.connect(options); //connect
+
+}
